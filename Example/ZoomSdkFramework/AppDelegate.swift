2fa7b7b4c9f5ee2729c5e2b8b16ee706a33c4575
Mohamed Abdul-Raouf <31677025+Mohamed-AbdulRaouf@users.noreply.github.com>
2022-04-03 13:33:55 +0200

    add arabic language

--- Example/ZoomSdkFramework/AppDelegate.swift
+++ Example/ZoomSdkFramework/AppDelegate.swift
@@ -14,15 +14,32 @@ import MobileRTC
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let sdkKey = <#sdkKey#>
+    let sdkSecret = <#sdkSecret#>
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let logger = Logger()
         logger.pringLog(log: "zoom init done")
+        setupSDK(sdkKey: sdkKey, sdkSecret: sdkSecret)
         return true
     }
 
+    func setupSDK(sdkKey: String, sdkSecret: String) {
+        let context = MobileRTCSDKInitContext()
+        context.domain = "zoom.us"
+        context.enableLog = true
+        context.appGroupId = ""
+        let sdkInitializedSuccessfully = MobileRTC.shared().initialize(context)
+        if sdkInitializedSuccessfully == true, let authorizationService = MobileRTC.shared().getAuthService() {
+            authorizationService.clientKey = sdkKey
+            authorizationService.clientSecret = sdkSecret
+            authorizationService.delegate = self
+            authorizationService.sdkAuth()
+        }
+        MobileRTC.shared().setLanguage("ar") // ar for Arabic language  , en for English
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -44,7 +61,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
+extension AppDelegate: MobileRTCAuthDelegate {
+    func onMobileRTCAuthReturn(_ returnValue: MobileRTCAuthError) {
+        switch returnValue {
+        case .success: //MobileRTCAuthError_Success:
+            print("SDK successfully initialized.")
+        case .keyOrSecretEmpty: //MobileRTCAuthError_KeyOrSecretEmpty:
+            assertionFailure("SDK Key/Secret was not provided. Replace sdkKey and sdkSecret at the top of this file with your SDK Key/Secret.")
+        case .keyOrSecretWrong, .unknown: //MobileRTCAuthError_KeyOrSecretWrong, MobileRTCAuthError_Unknown:
+            assertionFailure("SDK Key/Secret is not valid.")
+        default:
+            assertionFailure("SDK Authorization failed with MobileRTCAuthError: (returnValue).")
+        }
+    }
+}
--- Example/ZoomSdkFramework/ViewController.swift
+++ Example/ZoomSdkFramework/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MobileRTC
 
 class ViewController: UIViewController {
 
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        MobileRTC.shared().setMobileRTCRootController(self.navigationController)
+//        if #available(iOS 15.0, *) {
+//            printContent(MobileRTC.shared().supportedLanguages())
+//        } else {
+//            // Fallback on earlier versions
+//        }
+//        MobileRTC.shared().setLanguage("ru")
+//        MobileRTC.shared().setMobileRTCCustomLocalizableName("Localizable")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +29,79 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - IBOutlets
+    @IBAction func joinAMeetingButtonPressed(_ sender: UIButton) {
+        presentJoinMeetingAlert()
+    }
+    
+    func presentJoinMeetingAlert() {
+        let alertController = UIAlertController(title: "Join meeting", message: "", preferredStyle: .alert)
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Meeting number"
+            textField.keyboardType = .phonePad
+        }
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Meeting password"
+            textField.keyboardType = .asciiCapable
+            textField.isSecureTextEntry = true
+        }
+        let joinMeetingAction = UIAlertAction(title: "Join meeting", style: .default, handler: { alert -> Void in
+            let numberTextField = alertController.textFields![0] as UITextField
+            let passwordTextField = alertController.textFields![1] as UITextField
+
+            if let meetingNumber = numberTextField.text, let password = passwordTextField.text {
+                self.joinMeeting(meetingNumber: meetingNumber, meetingPassword: password)
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { (action : UIAlertAction!) -> Void in })
+        alertController.addAction(joinMeetingAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func startAnInstantMeetingButtonPressed(_ sender: UIButton) {
+        startMeetingZak()
+    }
+    
+        func joinMeeting(meetingNumber: String, meetingPassword: String) {
+                if let meetingService = MobileRTC.shared().getMeetingService() {
+                    meetingService.delegate = self
+                    let joinMeetingParameters = MobileRTCMeetingJoinParam()
+                    joinMeetingParameters.meetingNumber = meetingNumber
+                    joinMeetingParameters.password = meetingPassword
+                    meetingService.joinMeeting(with: joinMeetingParameters)
+                }
+            }
+    
+    func startMeetingZak() {
+        if let meetingService = MobileRTC.shared().getMeetingService() {
+            meetingService.delegate = self
+            let startMeetingParams = MobileRTCMeetingStartParam4WithoutLoginUser()
+            startMeetingParams.zak = "" // TODO: Enter ZAK
+            startMeetingParams.userID = "" // TODO: Enter userID
+            startMeetingParams.userName = "" // TODO: Enter your name
+            meetingService.startMeeting(with: startMeetingParams)
+        }
+    }
 }
 
+// 1. Extend the ViewController class to adopt and conform to MobileRTCMeetingServiceDelegate. The delegate methods will listen for updates from the SDK about meeting connections and meeting states.
+extension ViewController: MobileRTCMeetingServiceDelegate {
+    // Is called upon in-meeting errors, join meeting errors, start meeting errors, meeting connection errors, etc.
+    func onMeetingError(_ error: MobileRTCMeetError, message: String?) {
+        switch error {
+        case .passwordError: //MobileRTCMeetError_PasswordError:
+            print("Could not join or start meeting because the meeting password was incorrect.")
+        default:
+            print("Could not join or start meeting with MobileRTCMeetError: (error) \(message ?? "")")
+        }
+    }
+    // Is called when the user joins a meeting.
+    func onJoinMeetingConfirmed() {
+        print("Join meeting confirmed.")
+    }
+    // Is called upon meeting state changes.
+    func onMeetingStateChange(_ state: MobileRTCMeetingState) {
+        print("Current meeting state: (state)")
+    }
+}
